a31c1b46aeaed6de8699abf9e5b017a89612032b
Emily Velasco <evelasco@crcfe02.crc.nd.edu>
2023-10-12 14:44:23 -0400

    Adjusted combined sequence script

--- project_script.sh
+++ project_script.sh
@@ -1,8 +1,8 @@
 # Here we can compile all of the code so it will be easier to track
 # First, we have to combine the hsp70 and mcrA ref_sequences files
 
-cat hsp70gene_*.fasta >> combined_hsp70_refsequences.txt
-cat mcrAgene_*.fasta >>  combined_mcrA_refsequences.txt
+cat hsp70gene_*.fasta > combined_hsp70_refsequences.txt
+cat mcrAgene_*.fasta >  combined_mcrA_refsequences.txt
 
 #Once we have the combined files we can align the sequences
 # Not sure how we would run it for both the genes, or if that is necessary
@@ -15,7 +15,7 @@ cat mcrAgene_*.fasta >>  combined_mcrA_refsequences.txt
 ~/Private/Biocomputing/tools/hmmbuild ref_hsp70gene.hmm aligned_hsp70_refsequences.txt
 ~/Private/Biocomputing/tools/hmmbuild ref_mcrAgene.hmm  aligned_mcrA_refsequences.txt
 
-#Use hmmsearch to find the matches in the genome
+#Use hmmsearch to find the matches in the genome and make a table for the output
 # Should we  use a for loop for the proteome files?
 
 ~/Private/Biocomputing/tools/hmmsearch --tblout [output filename] *.hmm [genome filename]
--- ref_sequences/combined_hsp70_refsequences.sh
+++ ref_sequences/combined_hsp70_refsequences.sh
@@ -1,3 +1,3 @@
 # This will combine the ref_sequences for hsp70_gene
 
-cat hsp70gene_*.fasta >> combined_hsp70gene_refsequences.txt
+cat hsp70gene_*.fasta > combined_hsp70gene_refsequences.txt
