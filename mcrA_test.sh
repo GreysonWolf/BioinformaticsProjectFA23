6d9d8ace9a12c7558a61039b6e90c8859d3338a4
Emily Velasco <evelasco@crcfe02.crc.nd.edu>
2023-10-12 17:05:48 -0400

    WE DID IT BOYS

--- mcrA_test.sh
+++ mcrA_test.sh
@@ -6,13 +6,16 @@ cat ref_sequences/hsp70gene_*.fasta > combined_hsp70_refsequences.txt
 ~/Private/Biocomputing/tools/muscle -align combined_hsp70_refsequences.txt -output aligned_hsp70_refsequences.txt
 ~/Private/Biocomputing/tools/hmmbuild ref_hsp70gene.hmm aligned_hsp70_refsequences.txt
 
+let protNum=0
+echo "Proteome Number mcrA Matches hsp70 Matches" > mcrA_test.txt
 for genome in proteomes/proteome*.fasta
 do
-	echo -n "$genome " >> mcrA_test.txt
+	let protNum=$protNum+1
 	~/Private/Biocomputing/tools/hmmsearch --tblout $genome.moutput ref_mcrAgene.hmm $genome
-	cat $genome.moutput | grep -v "^#" | wc -l | tr -d '\n' >> mcrA_test.txt
+	mcrAMatch=$(cat $genome.moutput | grep -v "^#" | wc -l)
 	rm $genome.moutput
 	~/Private/Biocomputing/tools/hmmsearch --tblout $genome.houtput ref_hsp70gene.hmm $genome
-	cat $genome.houtput | grep -v "^#" | wc -l >> mcrA_test.txt
+	hsp70Match=$(cat $genome.houtput | grep -v "^#" | wc -l)
 	rm $genome.houtput
+	echo "proteome$protNum $mcrAMatch $hsp70Match" >> mcrA_test.txt
 done
